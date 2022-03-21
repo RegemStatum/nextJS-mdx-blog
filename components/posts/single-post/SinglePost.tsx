6f24aa860188f2e9282ae944a6dcd0ee7c51fad2
Oleksandr Kondratov <al.kon.workmail@gmail.com>
2022-03-21 12:23:50 +0200

    code highlight ready

--- components/posts/single-post/SinglePost.tsx
+++ components/posts/single-post/SinglePost.tsx
@@ -2,16 +2,30 @@ import React, { FC } from "react";
 import { MDXRemote } from "next-mdx-remote";
 // post components
 import Image from "next/image";
+import Highlight from "./Highlight";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 // styles
 import styles from "./SinglePost.module.scss";
-const components = {
-  Image,
-};
 
 interface SinglePostProps {
   meta: { [key: string]: any };
   source: any;
 }
+
+const code = ({ className, ...props }) => {
+  const match = /language-(\w+)/.exec(className || "");
+  return match ? (
+    <SyntaxHighlighter language={match[1]} PreTag="div" {...props} />
+  ) : (
+    <code className={className} {...props} />
+  );
+};
+
+const components = {
+  Image,
+  Highlight,
+  code,
+};
 
 const SinglePost: FC<SinglePostProps> = ({ meta, source }) => {
   return (
